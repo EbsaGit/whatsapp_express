4057b9fbe34c85e15bee353547d7e8b72fe40234
EbsaGit <zoho@asesoriazoho.com>
2024-07-23 13:45:09 -0400

    new update routes  endpoints

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const axios = require('axios');
 const { formatInTimeZone } = require('date-fns-tz');
 const app = express();
 const port = process.env.PORT || 3000;
+const MessageRoute = require('./src/routes/MessageRoute');
 
 const VERIFY_TOKEN = "holamundo94";
 
@@ -23,15 +24,10 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-// Ruta para obtener todos los mensajes ordenados por recipient_phone
-app.get('/messages', async (req, res) => {
-  try {
-    const messages = await Message.find().sort({ recipient_phone: 1 });
-    res.status(200).json(messages);
-  } catch (error) {
-    res.status(500).json({ error: 'Error al obtener los mensajes' });
-  }
-});
+//uso de las rutas
+
+app.use('/api',MessageRoute);
+
 
 // Endpoint para recibir y responder a los eventos de webhook
 app.post('/webhook', async (req, res) => {
@@ -83,69 +79,6 @@ app.post('/webhook', async (req, res) => {
     }
 });
 
-app.post('/api/message/send_save', async (req, res) => {
-    const body = req.body;
-
-    const text = body.text;
-    const phoneRecipiest = body.phoneRecipiest;
-    const Phone_Number_ID = body.Phone_Number_ID;
-    const display_phone_number = body.display_phone_number;
-    const accessToken = body.accessToken;
-
-    const url = `https://graph.facebook.com/v20.0/${Phone_Number_ID}/messages`;
-
-    const payload = {
-        messaging_product: "whatsapp",
-        recipient_type: "individual",
-        to: phoneRecipiest,
-        type: "text",
-        text: {
-            preview_url: true,
-            body: text,
-        },
-    };
-
-    try {
-        const response = await axios.post(url, payload, {
-            headers: {
-                "Content-Type": "application/json",
-                Authorization: `Bearer ${accessToken}`,
-            }
-        });
-
-        const data = response.data;
-        console.log("Mensaje enviado:", data);
-
-        try {
-            const createdTime = formatInTimeZone(
-                new Date(),
-                'America/Asuncion',
-                'yyyy-MM-dd HH:mm:ssXXX'
-            );
-
-            const newMessage = new Message({
-                recipient_phone: phoneRecipiest || 'unknown',  // Ajustar según tu estructura
-                message_id: data.messages[0].id,
-                display_phone_number: display_phone_number,
-                display_phone_number_id: Phone_Number_ID,
-                conversation_id: 'unknown',  // Ajustar según tu estructura
-                message_text: text,
-                type: "meta",
-                contact: "Corporativo",
-                created_time: createdTime  // Formateado a la zona horaria de Asunción, Paraguay
-            });
-            const guardarMensaje = await newMessage.save();
-            console.log(guardarMensaje);
-
-            res.sendStatus(200);
-        } catch (error) {
-            res.sendStatus(403);
-        }
-    } catch (error) {
-        console.error("Error al enviar el mensaje:", error);
-        res.sendStatus(403);
-    }
-});
 
 // Endpoint para la verificación del webhook
 app.get('/webhook', (req, res) => {
---
+++ src/routes/MessageRoute.js
@@ -0,0 +1,83 @@
+const express = require('express');
+const Message = require('../models/Message');
+const axios = require('axios');
+const MessageRoute = express.Router();
+const { formatInTimeZone } = require('date-fns-tz');
+
+// Ruta para obtener todos los mensajes ordenados por recipient_phone
+
+MessageRoute.get('/messages', async (req, res) => {
+    try {
+      const messages = await Message.find().sort({ recipient_phone: 1 });
+      res.status(200).json(messages);
+    } catch (error) {
+      res.status(500).json({ error: 'Error al obtener los mensajes' });
+    }
+ });
+
+ MessageRoute.post('/messages/send_save', async (req, res) => {
+    const body = req.body;
+
+    const text = body.text;
+    const phoneRecipiest = body.phoneRecipiest;
+    const Phone_Number_ID = body.Phone_Number_ID;
+    const display_phone_number = body.display_phone_number;
+    const accessToken = body.accessToken;
+
+    const url = `https://graph.facebook.com/v20.0/${Phone_Number_ID}/messages`;
+
+    const payload = {
+        messaging_product: "whatsapp",
+        recipient_type: "individual",
+        to: phoneRecipiest,
+        type: "text",
+        text: {
+            preview_url: true,
+            body: text,
+        },
+    };
+
+    try {
+        const response = await axios.post(url, payload, {
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${accessToken}`,
+            }
+        });
+
+        const data = response.data;
+        console.log("Mensaje enviado:", data);
+
+        try {
+            const createdTime = formatInTimeZone(
+                new Date(),
+                'America/Asuncion',
+                'yyyy-MM-dd HH:mm:ssXXX'
+            );
+
+            const newMessage = new Message({
+                recipient_phone: phoneRecipiest || 'unknown',  // Ajustar según tu estructura
+                message_id: data.messages[0].id,
+                display_phone_number: display_phone_number,
+                display_phone_number_id: Phone_Number_ID,
+                conversation_id: 'unknown',  // Ajustar según tu estructura
+                message_text: text,
+                type: "meta",
+                contact: "Corporativo",
+                created_time: createdTime  // Formateado a la zona horaria de Asunción, Paraguay
+            });
+            const guardarMensaje = await newMessage.save();
+            console.log(guardarMensaje);
+
+            res.sendStatus(200);
+        } catch (error) {
+            res.sendStatus(403);
+        }
+    } catch (error) {
+        console.error("Error al enviar el mensaje:", error);
+        res.sendStatus(403);
+    }
+});
+
+module.exports = MessageRoute;
+
